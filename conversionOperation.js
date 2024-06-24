e43e79e4f37cb0d2b8b11b79d7445975d8a5f7c3
Aadil Khan <aadilkhan99a@gmail.com>
2024-06-24 21:38:02 +0530

    conversion operations

---
+++ conversionOperation.js
@@ -0,0 +1,127 @@
+
+/* Number Conversion */
+
+// let score = "123"
+
+// let conversion = Number(score);
+// console.log(typeof conversion);
+// console.log(conversion);
+
+
+// score = "123abc"
+
+// console.log();
+
+// conversion = Number(score);
+// console.log(typeof conversion);
+// console.log(conversion);
+
+
+// score = null
+
+// console.log();
+
+// conversion = Number(score);
+// console.log(typeof conversion);
+// console.log(conversion);
+
+
+
+// score = undefined
+
+// console.log();
+
+// conversion = Number(score);
+// console.log(typeof conversion);
+// console.log(conversion);
+
+
+
+// score = true
+
+// console.log();
+
+// conversion = Number(score);
+// console.log(typeof conversion);
+// console.log(conversion);
+
+
+// score = ""
+
+// console.log();
+
+// conversion = Number(score);
+// console.log(typeof conversion);
+// console.log(conversion);
+
+
+// score = "aadil"
+
+// console.log();
+
+// conversion = Number(score);
+// console.log(typeof conversion);
+// console.log(conversion);
+
+
+// console.log();
+
+
+/*   Boolean Conversion  */
+
+let value = 1
+
+let boolenValue = Boolean(value);
+console.log(typeof boolenValue);
+console.log(boolenValue);
+
+
+console.log();
+
+value = ""
+
+boolenValue = Boolean(value);
+console.log(typeof boolenValue);
+console.log(boolenValue);
+
+
+console.log();
+
+value = "aadil"
+
+boolenValue = Boolean(value);
+console.log(typeof boolenValue);
+console.log(boolenValue);
+
+
+console.log();
+
+value = null
+
+boolenValue = Boolean(value);
+console.log(typeof boolenValue);
+console.log(boolenValue);
+
+
+
+console.log();
+
+value = undefined
+
+boolenValue = Boolean(value);
+console.log(typeof boolenValue);
+console.log(boolenValue);
+
+
+
+/*   String Conversion  */
+
+console.log();
+
+let num = 33;
+
+let stringNum = String(num);
+
+
+console.log(typeof stringNum);
+console.log(stringNum);
